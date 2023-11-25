be2144ded925b8ecff86c0b00857b654b310205c
Ixod3 <morgansoulard28@gmail.com>
2023-11-25 01:45:18 +0100

    rename random.py functions

--- fct_random.py
+++ fct_random.py
@@ -3,12 +3,16 @@
 # Import modules
 import random
 
-def rand_mac():
+def valid_mac_2():
     bytes_4 = hex(random.randint(0, 254))[2:]
     bytes_5 = hex(random.randint(0, 254))[2:]
     bytes_6 = hex(random.randint(0, 254))[2:]
     random_mac = str(f"fc:3c:2c:{bytes_4}:{bytes_5}:{bytes_6}")
 
-def rand_transaction_id():
+def transaction_id():
     random_transaction_id = random.randint(0, 0xFFFFFFF)
     return random_transaction_id
+
+def valid_mac():
+    return "44:af:28:d9:46:a2"
+    #return "2c:6d:c1:ab:6e:f9"
