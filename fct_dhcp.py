9e3eeaed72f7f4bac5218d1a5cd087e75ec5628e
Ixod3 <morgansoulard28@gmail.com>
2023-11-25 01:43:32 +0100

    fix DHCP discovery options value

--- fct_dhcp.py
+++ fct_dhcp.py
@@ -11,7 +11,7 @@ White = "\033[0m"
 def discover(src_mac, xid, interface):
     # Forge packet
     src_mac_bytes = (int(src_mac.replace(":", ""), 16).to_bytes(6, "big"))
-    dhcp_discover = scapy.Ether(src=src_mac) / scapy.IP(src="0.0.0.0", dst="255.255.255.255") / scapy.UDP(sport=68, dport=67) / scapy.BOOTP(chaddr=src_mac_bytes, xid=xid, flags=0x0000) / scapy.DHCP(options=[("message-type", "discover"), ('client_id', '44:af:28:d9:46:a3'), ('param_req_list',[1,2,3,6,12,15,26,28,33,40,41,42,119,121]),('hostname',f"user_X"), "end"])
+    dhcp_discover = scapy.Ether(src=src_mac,dst="ff:ff:ff:ff:ff:ff") / scapy.IP(src="0.0.0.0", dst="255.255.255.255") / scapy.UDP(sport=68, dport=67) / scapy.BOOTP(chaddr=src_mac_bytes, xid=xid, flags=0x0000) / scapy.DHCP(options=[("message-type", "discover"), ('client_id', src_mac), ('param_req_list',[1,2,3,6,12,15,26,28,33,40,41,42,119,121]),('hostname',f"user_X"), "end"])
     # Send
     scapy.sendp(dhcp_discover, iface=interface, verbose=0)
     # Output
