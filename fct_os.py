50a4ed298bcb0550101a75c72b2ce773eb26c4c7
Ixod3 <morgansoulard28@gmail.com>
2023-11-26 22:42:31 +0100

    get interface ip

--- fct_os.py
+++ fct_os.py
@@ -12,4 +12,22 @@ def get_mac(interface):
 
 def set_promiscuous(interface):
     subprocess.run(f"sudo ifconfig {interface} promisc 2>&1 > /dev/null", shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, text=True)
-    time.sleep(3)
+    time.sleep(2)
+
+def interface_ip(interface):
+    cmd_return = subprocess.check_output(f"ip a l {interface}", shell=True, stderr=subprocess.STDOUT, text=True)
+    own_ip = cmd_return.split("inet ")[1]
+    own_ip = own_ip.split(" brd")[0]
+    cidr = int(own_ip.split("/")[1])
+    own_ip = own_ip.split("/")[0]
+    if cidr == 24:
+        network_id = f"{own_ip.split('.')[0]}.{own_ip.split('.')[1]}.{own_ip.split('.')[2]}."
+        host_number = 254
+    elif cidr == 16:
+        network_id = f"{own_ip.split('.')[0]}.{own_ip.split('.')[1]}."
+        host_number = 65534
+    elif cidr == 8:
+        network_id = f"{own_ip.split('.')[0]}."
+        host_number = 16777214
+    return  own_ip, network_id, host_number
+
